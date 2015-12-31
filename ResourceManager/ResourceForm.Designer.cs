277e87760e9db065c7597bbee66847917571c9e3
Vo Quang Hoa <voquanghoa@gmail.com>
2015-12-31 13:12:14 +0700

    Init project

---
+++ ResourceManager/ResourceForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ResourceManager
+{
+	partial class ResourceForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.TxtDataPath = new System.Windows.Forms.TextBox();
+			this.BtnSelectDataPath = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.TxtJsonPath = new System.Windows.Forms.TextBox();
+			this.BtnOpenJsonFile = new System.Windows.Forms.Button();
+			this.DialogOpenFile = new System.Windows.Forms.OpenFileDialog();
+			this.DialogSelectFolder = new System.Windows.Forms.FolderBrowserDialog();
+			this.BtnLoadData = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(11, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(59, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Root folder";
+			// 
+			// TxtDataPath
+			// 
+			this.TxtDataPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.TxtDataPath.Location = new System.Drawing.Point(76, 17);
+			this.TxtDataPath.Name = "TxtDataPath";
+			this.TxtDataPath.Size = new System.Drawing.Size(415, 20);
+			this.TxtDataPath.TabIndex = 1;
+			this.TxtDataPath.Text = "D:\\Project\\VEnglish\\app\\src\\main\\assets";
+			// 
+			// BtnSelectDataPath
+			// 
+			this.BtnSelectDataPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.BtnSelectDataPath.Location = new System.Drawing.Point(503, 14);
+			this.BtnSelectDataPath.Name = "BtnSelectDataPath";
+			this.BtnSelectDataPath.Size = new System.Drawing.Size(75, 23);
+			this.BtnSelectDataPath.TabIndex = 2;
+			this.BtnSelectDataPath.Text = "Open";
+			this.BtnSelectDataPath.UseVisualStyleBackColor = true;
+			this.BtnSelectDataPath.Click += new System.EventHandler(this.BtnSelectDataPath_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 52);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(29, 13);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Json";
+			// 
+			// TxtJsonPath
+			// 
+			this.TxtJsonPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.TxtJsonPath.Location = new System.Drawing.Point(76, 49);
+			this.TxtJsonPath.Name = "TxtJsonPath";
+			this.TxtJsonPath.Size = new System.Drawing.Size(415, 20);
+			this.TxtJsonPath.TabIndex = 1;
+			// 
+			// BtnOpenJsonFile
+			// 
+			this.BtnOpenJsonFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.BtnOpenJsonFile.Location = new System.Drawing.Point(503, 46);
+			this.BtnOpenJsonFile.Name = "BtnOpenJsonFile";
+			this.BtnOpenJsonFile.Size = new System.Drawing.Size(75, 23);
+			this.BtnOpenJsonFile.TabIndex = 2;
+			this.BtnOpenJsonFile.Text = "Open";
+			this.BtnOpenJsonFile.UseVisualStyleBackColor = true;
+			this.BtnOpenJsonFile.Click += new System.EventHandler(this.BtnOpenJsonFile_Click);
+			// 
+			// DialogOpenFile
+			// 
+			this.DialogOpenFile.CheckFileExists = false;
+			this.DialogOpenFile.CheckPathExists = false;
+			this.DialogOpenFile.Filter = "Json file (*.json)|*.json";
+			// 
+			// BtnLoadData
+			// 
+			this.BtnLoadData.Location = new System.Drawing.Point(421, 80);
+			this.BtnLoadData.Name = "BtnLoadData";
+			this.BtnLoadData.Size = new System.Drawing.Size(157, 36);
+			this.BtnLoadData.TabIndex = 4;
+			this.BtnLoadData.Text = "Load data";
+			this.BtnLoadData.UseVisualStyleBackColor = true;
+			this.BtnLoadData.Click += new System.EventHandler(this.BtnLoadData_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(21, 91);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(180, 17);
+			this.checkBox1.TabIndex = 5;
+			this.checkBox1.Text = "Distinct and no extendsion mode";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// ResourceForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(590, 128);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.BtnLoadData);
+			this.Controls.Add(this.BtnOpenJsonFile);
+			this.Controls.Add(this.BtnSelectDataPath);
+			this.Controls.Add(this.TxtJsonPath);
+			this.Controls.Add(this.TxtDataPath);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "ResourceForm";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox TxtDataPath;
+		private System.Windows.Forms.Button BtnSelectDataPath;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox TxtJsonPath;
+		private System.Windows.Forms.Button BtnOpenJsonFile;
+		private System.Windows.Forms.OpenFileDialog DialogOpenFile;
+		private System.Windows.Forms.FolderBrowserDialog DialogSelectFolder;
+		private System.Windows.Forms.Button BtnLoadData;
+		private System.Windows.Forms.CheckBox checkBox1;
+	}
+}
+
---
+++ ResourceManager/ResourceForm.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ResourceManager
+{
+	public partial class ResourceForm : Form, IComparer<String>
+	{
+		private const string DefaultDataFile = "data.json";
+		
+		public ResourceForm()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnOpenJsonFile_Click(object sender, EventArgs e)
+		{
+			if (DialogOpenFile.ShowDialog() != DialogResult.Cancel)
+			{
+				TxtJsonPath.Text = DialogOpenFile.FileName;
+			}
+		}
+
+		private void BtnSelectDataPath_Click(object sender, EventArgs e)
+		{
+			DialogSelectFolder.SelectedPath = TxtDataPath.Text;
+            if (DialogSelectFolder.ShowDialog() != DialogResult.Cancel)
+			{
+				TxtDataPath.Text = DialogSelectFolder.SelectedPath;
+				if (TxtJsonPath.TextLength == 0)
+				{
+					TxtJsonPath.Text = Path.Combine(DialogSelectFolder.SelectedPath, DefaultDataFile);
+                }
+			}
+		}
+
+		private void BtnLoadData_Click(object sender, EventArgs e)
+		{
+			if (TxtDataPath.TextLength == 0 || TxtJsonPath.TextLength == 0)
+			{
+				ShowError("Chưa chọn đường dẫn này nọ !!!");
+			}
+			else
+			{
+				var realData = LoadDir(TxtDataPath.Text, 0);
+				var jsonData = LoadJson(TxtJsonPath.Text);
+				MergeDataItem(realData, jsonData);
+				SaveJson(TxtJsonPath.Text, realData);
+				ShowInfo("Đã lưu ra file !!!");
+			}
+        }
+
+		private void MergeDataItem(DataItem realData, DataItem jsonData)
+		{
+			if (realData != null && jsonData != null)
+			{
+				if(!string.IsNullOrEmpty(jsonData.Display))
+				{
+					realData.Display = jsonData.Display;
+				}
+
+				if (realData.Children != null && jsonData.Children!=null)
+				{
+					foreach (var child in realData.Children)
+					{
+						var jsonChild = jsonData.Children.FirstOrDefault(x => string.Equals(x.FileName, child.FileName));
+						MergeDataItem(child, jsonChild);
+					}
+				}
+			}
+		}
+
+		private DataItem LoadDir(string path, int level)
+		{
+			var dataItem = new DataItem();
+
+			dataItem.FileName = Path.GetFileName(path);
+			dataItem.Display = DefaultFormat(dataItem.FileName, level>0);
+
+			if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
+			{
+				var files = Directory.EnumerateFileSystemEntries(path).Where(x => !x.EndsWith(DefaultDataFile)).ToList().OrderBy(x => x, this);
+
+				dataItem.Children = files.Select(x => LoadDir(Path.Combine(path, x), level + 1)).ToList();
+			}
+
+			return dataItem;
+		}
+
+		private string DefaultFormat(string fileName, bool toLower)
+		{
+			var withoutExt = Path.GetFileNameWithoutExtension(fileName);
+			var withSpace = withoutExt.Replace('_', ' ');
+			if (toLower)
+			{
+				withSpace = withSpace.ToLower();
+			}
+			var capital = char.ToUpper(withSpace[0]) + withSpace.Substring(1);
+			return capital;
+		}
+
+		private DataItem LoadJson(string fileName)
+		{
+			try
+			{
+				return JsonConvert.DeserializeObject<DataItem>(File.ReadAllText(fileName));
+			}
+			catch
+			{
+				return new DataItem();
+			}
+		}
+
+		private void SaveJson(string fileName, DataItem dataItem)
+		{
+			try
+			{
+				File.WriteAllText(fileName, JsonConvert.SerializeObject(dataItem));
+			}
+			catch (Exception ex)
+			{
+				ShowError(ex.Message);
+            }
+		}
+
+		private void ShowError(string error)
+		{
+			MessageBox.Show(error, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+		}
+
+		private void ShowInfo(string error)
+		{
+			MessageBox.Show(error, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		public int Compare(string x, string y)
+		{
+			x = x.Trim();
+			y = y.Trim();
+
+			if (x.LastIndexOf('.') > 0)
+			{
+				x = x.Substring(0, x.LastIndexOf('.'));
+			}
+
+			if (y.LastIndexOf('.') > 0)
+			{
+				y = y.Substring(0, y.LastIndexOf('.'));
+			}
+
+			int xLastSpace = x.LastIndexOf(' ');
+			int yLastSpace = y.LastIndexOf(' ');
+
+			if (xLastSpace < 0)
+			{
+				xLastSpace = x.LastIndexOf('_');
+			}
+
+			if (yLastSpace < 0)
+			{
+				yLastSpace = y.LastIndexOf('_');
+            }
+
+			try
+			{
+				if (yLastSpace >= 0 && xLastSpace >= 0)
+				{
+					string strNumX = x.Substring(xLastSpace + 1);
+					string strNumY = y.Substring(yLastSpace + 1);
+
+					int numX = int.Parse(strNumX);
+					int numY = int.Parse(strNumY);
+
+					return numX - numY;
+				}
+			}
+			catch
+			{
+				Console.WriteLine(x + " -- " + y);
+			}
+			return x.CompareTo(y);
+		}
+	}
+}
